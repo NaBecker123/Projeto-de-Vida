6e45a43dc470b91c25ca53c6c3658429ffb7494b
NaBecker123 <115232006+NaBecker123@users.noreply.github.com>
2024-05-17 12:08:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0; i<botoes.length; i=i+1){
+    botoes[i].onclinck = function(){
+        botoes[i].classList.add("ativo")
+    }
+    console.log(i);
+}
+
